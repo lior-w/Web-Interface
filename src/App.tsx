35d23c934bdc993a87bc9682fc2754a9eced5104
Lior <wlior@post.bgu.ac.il>
2024-02-20 21:37:48 +0200

    tailwindcss install

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import Login from "./components/Login";
-import Registration from "./components/Registration";
-import CreateGame from "./components/CreateGame";
 import SavedGames from "./components/SavedGames";
-import CreateQuestionnaire from "./components/CreateQuestionaire";
-import { Router } from "react-router";
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SavedGames />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  return <SavedGames />;
 }
 
 export default App;
