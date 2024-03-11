0cdc5bbd9f65df5bb03ff53d916228144193fcea
Roei Birnfeld <roeibir@post.bgu.ac.il>
2024-03-11 13:41:49 +0200

    added tile type and get the map from the server

--- src/components/countriesMap.tsx
+++ src/components/countriesMap.tsx
@@ -1,71 +1,76 @@
 import * as React from "react";
-import { useState } from "react";
-import { CountriesMap, Country, CountryStatus } from "../types";
+import { useEffect, useState } from "react";
+import { CountriesMap, Country, CountryStatus, Tile, Token } from "../types";
 import { CountryComp } from "./country";
 import Container from "./container";
+import axios from "axios";
 
 export interface IProps {
   countriesMap: CountriesMap;
+  gameRunningId: string;
+  token: Token;
 }
 
-export const CountriesMapComp = ({ countriesMap }: IProps) => {
-  const [showQuestion, setShowQuestion] = useState(false);
-  const [selectedCountry, setSelectedCountry] = useState<Country>();
+export const CountriesMapComp = ({
+  countriesMap,
+  gameRunningId,
+  token,
+}: IProps) => {
+  const [tiles, setTiles] = useState<Tile[]>([]);
 
-  const [countryData, setCountryData] = useState<Country[]>(
-    countriesMap.countries
-  );
-
-  const handleCountryPress = (country: Country) => {
-    //setShowQuestion(true);
-    //setSelectedCountry(country);
-    console.log(country.name);
+  const getMapChanges = async () => {
+    await axios
+      .get(`http://localhost:8080/running_game/refresh_map/${gameRunningId}`, {
+        headers: { Authorization: token.AUTHORIZATION },
+      })
+      .then((res) => {
+        setTiles(res.data.value.tiles);
+      })
+      .catch((err) => console.log(err));
   };
 
-  const handleAnswerRight = (country: Country) => {
-    const newCountry = { ...country, status: CountryStatus.Selected };
-    const newCountryData = [
-      ...countryData.filter((c) => c.id !== country.id),
-      newCountry,
-    ];
-    setCountryData(newCountryData);
-    setShowQuestion(false);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+
+  const startInterval = () => {
+    const id = setInterval(getMapChanges, 1000);
+    setIntervalId(id);
   };
 
-  const handleAnswerFalse = () => {
-    setShowQuestion(false);
+  const stopInterval = () => {
+    if (intervalId !== null) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
   };
+
+  useEffect(() => {
+    startInterval();
+  }, []);
 
   return (
     <>
-      {!showQuestion && (
-        <Container w="auto" h="auto">
-          <div className="border-1 border-black flex flex-col items-center">
-            <div className="text-brown text-6xl text-center font-bold p-8">
-              {countriesMap.name}
-            </div>
-
-            <svg
-              className="p-8 max-w-[1000px] border-1"
-              height={"85%"}
-              viewBox={"150 30 490 520"}
-              width={"95%"}
-              id="svg"
-              strokeLinejoin="round"
-              stroke="#000"
-              fill="none"
-            >
-              {countryData.map((country) => (
-                <CountryComp
-                  key={country.id}
-                  country={country}
-                  onClick={() => handleCountryPress(country)}
-                />
-              ))}
-            </svg>
+      <Container w="auto" h="auto">
+        <div className="border-1 border-black flex flex-col items-center">
+          <div className="text-brown text-6xl text-center font-bold p-8">
+            {countriesMap.name}
           </div>
-        </Container>
-      )}
+
+          <svg
+            className="p-8 max-w-[1000px] border-1"
+            height={"85%"}
+            viewBox={"150 30 490 520"}
+            width={"95%"}
+            id="svg"
+            strokeLinejoin="round"
+            stroke="#000"
+            fill="none"
+          >
+            {Object.entries(tiles).map(([key, tile]) => (
+              <CountryComp key={key} tile={tile} onClick={() => {}} />
+            ))}
+          </svg>
+        </div>
+      </Container>
     </>
   );
 };
--- src/types.ts
+++ src/types.ts
@@ -1,75 +1,85 @@
 export enum CountryStatus {
-    Normal = "Normal",
-    Selected = "Selected",
-    Conquered = "Conquered",
-  }
+  Normal = "Normal",
+  Selected = "Selected",
+  Conquered = "Conquered",
+}
 
 export interface Country {
-    id: string;
-    name: string;
-    d: string;
-    strokeWidth: number;
-    fill: string;
-    status: CountryStatus;
-    controllingGroup?: number;
-  }
+  id: string;
+  name: string;
+  d: string;
+  strokeWidth: number;
+  fill: string;
+  status: CountryStatus;
+  controllingGroup?: number;
+}
 
-  export interface MapViewSizes {
-    height: string;
-    width: string;
-    viewBox: string;
-  }
+export interface MapViewSizes {
+  height: string;
+  width: string;
+  viewBox: string;
+}
 
-  export interface CountriesMap {
-    name: string;
-    countries: Country[];
-    mapSizes: MapViewSizes;
-  }
+export interface CountriesMap {
+  name: string;
+  countries: Country[];
+  mapSizes: MapViewSizes;
+}
 
-  export interface Token {
-    AUTHORIZATION: string;
-  }
+export interface Token {
+  AUTHORIZATION: string;
+}
 
-  export interface Question {
-    id: string;
-    multipleChoice: boolean;
-    question: string;
-    answer: string;
-    incorrectAnswers: string[];
-    difficulty: number;
-    tags: string[];
-  }
+export interface Question {
+  id: string;
+  multipleChoice: boolean;
+  question: string;
+  answer: string;
+  incorrectAnswers: string[];
+  difficulty: number;
+  tags: string[];
+}
 
-  export interface Questionaire {
-    id: string;
-    name: string;
-    questions: Question[];
-  }
+export interface Questionaire {
+  id: string;
+  name: string;
+  questions: Question[];
+}
 
-  export interface User {
-    id: string;
-    username: string;
-    email: string;
-    permissions: string;
-  }
+export interface User {
+  id: string;
+  username: string;
+  email: string;
+  permissions: string;
+}
 
-  export interface Map {
-    id: string;
-    name: string;
-    statringPositions: string[];
-  }
+export interface Map {
+  id: string;
+  name: string;
+  statringPositions: string[];
+}
 
-  export interface Game {
-    id: string;
-    name: string;
-    host: User;
-    description: string;
-    questionnaire: Questionaire;
-    map: Map;
-    numberOfGroups: number;
-    status: string;
-    groupAssignmentProtocol: string;
-    gameTime: number;
-    questionTimeLimit: number;
-    shared: boolean;
-  }
+export interface Game {
+  id: string;
+  name: string;
+  host: User;
+  description: string;
+  questionnaire: Questionaire;
+  map: Map;
+  numberOfGroups: number;
+  status: string;
+  groupAssignmentProtocol: string;
+  gameTime: number;
+  questionTimeLimit: number;
+  shared: boolean;
+  runningId: string;
+  tiles: Tile[];
+}
+
+export interface Tile {
+  id: string;
+  dimensions: string;
+  tileType: string;
+  controllingGroup: number;
+  difficultyLevel: number;
+}
