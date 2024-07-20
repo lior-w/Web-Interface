ca98bbb2244ef1a2d3fdfddc79ac0b4e880d2ee6
Lior <wlior@post.bgu.ac.il>
2024-07-20 17:37:23 +0300

    localhost ip

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { App } from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 
-const servers = ["http://172.31.8.223:8080", "http://127.0.0.1:8080"];
+const servers = ["http://localhost:8080", "http://127.0.0.1:8080"];
 export const server = servers[0];
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
