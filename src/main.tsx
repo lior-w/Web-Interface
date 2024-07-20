26a7dbd2a4fe061fceb410e5f2db23eb9c98354b
Lior <wlior@post.bgu.ac.il>
2024-07-20 17:24:49 +0300

    127.0.0.1

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { App } from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 
-const servers = ["http://13.59.100.152:8080", "http://localhost:8080"];
+const servers = ["http://13.59.100.152:8080", "http://127.0.0.1:8080"];
 export const server = servers[1];
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
